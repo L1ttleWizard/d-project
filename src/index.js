800114e56f9899fbd1dccc92a5b2f8ce683885a3
l1ttleWizard <andrwyandreev2005@gmail.com>
2023-03-10 00:51:13 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import _ from 'lodash'
+import './scss/styles.scss'
+import "./font.js"
+
+
+// or, specify which plugins you need:
+import { Tooltip, Toast, Popover } from 'bootstrap'
+import bootstrap from 'bootstrap'
+
+document.querySelector('.thed').addEventListener('click',()=>{
+    var btnSwitch = document.getElementById('btnSwitch');
+    var body = document.querySelector('html');
+    console.log('click');
+    if (body.getAttribute('data-bs-theme')=='dark') {
+        body.setAttribute('data-bs-theme', 'light');
+        document.getElementById('btnSwitch').textContent="Switch to Dark Theme";
+
+        
+        
+    }
+    else {
+        body.setAttribute('data-bs-theme', 'dark');
+        document.getElementById('btnSwitch').textContent="Switch to Light Theme";
+
+    }
+});
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+
